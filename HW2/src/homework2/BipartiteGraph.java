1ca7a04cdcba8f4c153f0f9994796e7bb24e4466
noga <nogigo@gmail.com>
2019-12-28 14:00:10 +0200

    update node and bipartiteGraph

--- HW2/src/homework2/BipartiteGraph.java
+++ HW2/src/homework2/BipartiteGraph.java
@@ -1,4 +1,159 @@
 package homework2;
 
-public class BipartiteGraph {
+import java.util.*;
+
+/**
+ * This class implements BipartiteGraph.
+ * Each node and edge in the graph has a label of type L.
+ * Each node's label is uniq.
+ * Each edge can only connect nodes of different types.
+ */
+public class BipartiteGraph<L, D> {
+    // Abstraction function:
+
+    // Representation Invariant:
+
+    private Map<L, Node<D>> nodes; // L for label
+    /**
+     * @modifies this
+     * @effects Constructs a new graph.
+     */
+    public BipartiteGraph() {
+        nodes = new HashMap<L, Node<D>>();
+        checkRep();
+    }
+
+
+    /**
+     * @requires nodeLabel != null
+     * @modifies graph
+     * @effects Adds a black or white node represented by nodeName label
+     * to the graph.
+     */
+    public void addNode(Object nodeLabel, D data, NodeColor type) {
+        // TODO: Implement this method
+        checkRep();
+        if (nodeLabel == null) {
+            throw new BadGraphHandleException("Cannot add a node with " +
+                    "null nodeName");
+        }
+        Node<D> newNode = new Node<D>(data, type);
+        nodes.put((L)nodeLabel, newNode);
+    }
+
+    /**
+     * @requires ((addBlackNode(parentLabel) && addWhiteNode(childLabel))
+     *              || (addWhiteNode(parentLabel) && addBlackNode(childLabel)))
+     *           && edgeLabel != null
+     *           && node named parentLabel has no other outgoing edge labeled
+     * 				edgeLabel
+     *           && node named childLabel has no other incoming edge labeled
+     * 				edgeLabel
+     * @modifies graph named graphName
+     * @effects Adds an edge from the node parentLabel to the node childLabel
+     * 			in the graph. The new edge's label is the Object
+     * 			edgeLabel.
+     */
+    public void addEdge(L parentLabel, L childLabel,
+                        L edgeLabel) {
+        //TODO: Implement this method
+
+        if (edgeLabel == null || parentLabel == childLabel )  {
+
+        }
+        Node parentNode = nodes.get(parentLabel);
+        Node childNode = nodes.get(childLabel);
+        if (parentNode.getColor() == childNode.getColor()) {
+
+        }
+        if()
+    }
+
+    /**
+     * @requires
+     * @return a space-separated list of the names of all the black nodes
+     * 		   in the graph, in alphabetical order.
+     */
+    public Object listBlackNodes() {
+        //TODO: Implement this method
+
+    }
+
+    /**
+     * @requires
+     * @return a space-separated list of the names of all the white nodes
+     * 		   in the graph, in alphabetical order.
+     */
+    public Object listWhiteNodes() {
+        //TODO: Implement this method
+
+    }
+
+    /**
+     * @requires createNode(parentName)
+     * @return a space-separated list of the names of the children of
+     * 		   parentName in the graph, in alphabetical order.
+     */
+    public Object listChildren(Object parentName) {
+        //TODO: Implement this method
+    }
+
+    /**
+     * @requires createNode(childName)
+     * @return a space-separated list of the names of the parents of
+     * 		   childName in the graph, in alphabetical order.
+     */
+    public Object listParents(Object childName) {
+        //TODO: Implement this method
+
+    }
+
+
+    /**
+     * @requires addEdge(graphName, parentName, str, edgeLabel) for some
+     * 			 string str
+     * @return the name of the child of parentName that is connected by the
+     * 		   edge labeled edgeLabel, in the graph graphName.
+     */
+    public Object getChildByEdgeLabel(Object parentName, Object edgeLabel) {
+        //TODO: Implement this method
+
+
+    }
+
+
+    /**
+     * @requires addEdge(graphName, str, childName, edgeLabel) for some
+     * 			 Object obj
+     * @return the name of the parent of childName that is connected by the
+     * 		   edge labeled edgeLabel, in the graph.
+     */
+    public Object getParentByEdgeLabel(Object childName, Object edgeLabel) {
+        //TODO: Implement this method
+
+
+    }
+
+    /*--------------------------------ADD FUNCTIONS----------------------------------*/
+
+    /**
+     *
+     */
+    public boolean isConnected(Object srcNode, Object dstNode) {
+
+    }
+
+    /**
+     * @return list of all the nodes between srcNode and dstNode.
+     */
+    public List<Object> getPath(Object srcNode, Object dstNode) {
+
+    }
+
+    /**
+     * Check rep.
+     */
+    private void checkRep() {
+
+    }
 }
--- HW2/src/homework2/Node.java
+++ HW2/src/homework2/Node.java
@@ -17,28 +17,29 @@ enum NodeColor {
  */
 public class Node<L,D> {
     // Abstraction function:
+    // A Node n contains data (of type D) and color of type NodeColor.
 
-    // Representation Invariant:
+    // Representation Invariant for every Node n:
+    // (n.color == BLACK || n.color == WHITE) &&
+    //
 
     private final NodeColor color;
     private final D data;
 
-    private List<Node<L, D>> parentsList = new ArrayList<>();
-    private List<Node<L, D>> childrenList = new ArrayList<>();
-    private Map<D, Edge<L>> inEdges = new HashMap<D, Edge<L>>();
-    private Map<D, Edge<L>> outEdges = new HashMap<D, Edge<L>>();
+    private List<Node<L, D>> parentsList = new ArrayList<Node<L, D>>();
+    private List<Node<L, D>> childrenList = new ArrayList<Node<L, D>>();
+    private Map<L, Edge<L>> inEdges = new HashMap<L, Edge<L>>();
+    private Map<L, Edge<L>> outEdges = new HashMap<L, Edge<L>>();
 
     /**
-     * Constructor.
-     *
+     * Constructs a new node
      * @modifies this
      * @effects Instantiates a new node.
-     * @throws BadGraphHandleException
-     *             when invoked with null parameters
+     * @throws NullPointerException when invoked with null parameters
      */
-    public Node(D data, NodeColor color) throws BadGraphHandleException {
+    public Node(D data, NodeColor color) throws NullPointerException {
         if (data == null || color == null) {
-            throw new BadGraphHandleException();
+            throw new NullPointerException();
         }
         this.color = color;
         this.data = data;
@@ -46,37 +47,59 @@ public class Node<L,D> {
     }
 
     /**
-     *
+     * @return the nodes color
      */
     public NodeColor getColor() {
         return this.color;
     }
 
     /**
-     *
+     * @return the node data
      */
     public D getNodeData() {
         return this.data;
     }
 
     /**
-     *
+     * @return copy of the map of outgoing edges.
      */
-    public NodeColor getOutgoingEdges() {
-        return this.color;
+    public HashMap<L, Edge<L>> getOutgoingEdges() {
+        HashMap<L, Edge<L>>copyOfOutEdges = new HashMap<L, Edge<L>>(outEdges);
+        return copyOfOutEdges;
     }
 
     /**
-     *
+     * @return copy of the map of ingoing edges.
      */
-    public NodeColor getIncomingEdges() {
-        return this.color;
+    public HashMap<L, Edge<L>> getIncomingEdges() {
+        HashMap<L, Edge<L>>copyOfInEdges = new HashMap<L, Edge<L>>(inEdges);
+        return copyOfInEdges;
+    }
+
+    /**
+     * @return copy of list of parent nodes.
+     */
+    public List<Node<L, D>> getNodesParents() {
+        ArrayList<Node<L, D>> copyOfParentsList =
+                new ArrayList<Node<L, D>>(parentsList);
+        return copyOfParentsList;
+    }
+
+    /**
+     * @return copy of list of child nodes.
+     */
+    public List<Node<L, D>> getNodesChildren() {
+        ArrayList<Node<L, D>> copyOfChildrenList =
+                new ArrayList<Node<L, D>>(childrenList);
+        return copyOfChildrenList;
     }
 
     /**
      * Check rep.
      */
     private void checkRep() {
+        assert (this.color == NodeColor.BLACK || this.color == NodeColor.WHITE) :
+                "Nodes color is illegal";
 
     }
 }
