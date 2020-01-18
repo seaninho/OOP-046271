996705b5e8c252667df08473cc5baa9a5975450f
noga <nogigo@gmail.com>
2020-01-19 00:05:39 +0200

    start

--- HW4/src/homework4/ColorGenerator.java
+++ HW4/src/homework4/ColorGenerator.java
@@ -8,5 +8,9 @@ public class ColorGenerator {
     //	 A ColorGenerator is represented by its color and changing method.
     //
     //	 Representation Invariant:
-    //	 changing method is one of four methods.
+    //	 The color update method is one of four methods:
+    //	1) Ascending order
+    //  2) Change by columns
+    //  3) Update in two steps
+    //  4) Update randomly
 }
