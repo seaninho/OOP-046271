568396d8c862326a4dc3f9355b581ae8d6c5f098
noga <nogigo@gmail.com>
2020-01-26 23:23:03 +0200

    get next panel

--- HW4/src/homework4/Billboard.java
+++ HW4/src/homework4/Billboard.java
@@ -1,21 +1,32 @@
 package homework4;
 
+import javax.swing.*;
+
 /**
- * Billboard represents an electronic billboard.
+ * Billboard represents an billboard of size (WINDOW_HEIGHT x WINDOW_WIDTH).
+ * Billboard class is a JFrame and it contain a grid of
+ * (NUM_OF_HEIGHT_PANELS x NUM_OF_WIDTH_PANELS) panels instances.
+ * The panels color is set by colorGenerator
  */
-public class Billboard {
+public class Billboard extends JFrame {
     //	 Abstraction function:
-    //	 A billboard is represented a set of 25 panels, arranged in a 5X5 grid.
+    //	 A billboard is represented a set of BOARDSIZE panels, arranged in a
+    //	 (NUM_OF_HEIGHT_PANELS X NUM_OF_WIDTH_PANELS) grid.
     //
     //	 Representation Invariant:
     //	 there are 25 panels.
 
 
-    // Billboard ...:
     private static final int MAX_COLOR = 255;
-    private static final int WINDOW_SIZE = 600;
-    private static final int NUM_OF_PANELS = 5;
-    private static final int BILL_HEIGHT = 5;
+    private static final int WINDOW_HEIGHT = 600;
+    private static final int WINDOW_WIDTH = 600;
+    private static final int NUM_OF_HEIGHT_PANELS = 5;
+    private static final int NUM_OF_WIDTH_PANELS = 5;
+    private final int BOARDSIZE = NUM_OF_HEIGHT_PANELS*NUM_OF_WIDTH_PANELS
+
+    JPanel billboard = new JPanel();
+    Panel panels[] = new Panel[BOARDSIZE];
+    ColorGenerator colorGenerator = ColorGenerator.getInstance();
 
 
 }
